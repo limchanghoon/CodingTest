1ab0bc29d4e7141c8432b3bd8a8f6e7403697c50
limchanghoon <52607063+limchanghoon@users.noreply.github.com>
2022-06-26 14:49:31 +0900

    프로그래머스 레벨 3 [베스트앨범]
     프로그래머스 레벨 3 [베스트앨범]

---
+++ LEVEL 3/L3_베스트앨범.cpp
@@ -0,0 +1,72 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+bool cmp_si(const pair<string, int>& a, const pair<string, int>& b) {
+    //if (a.second == b.second) return a.first > b.first;
+    return a.second > b.second;
+}
+
+bool cmp_ii(const pair<int, int>& a, const pair<int, int>& b) {
+    //if (a.second == b.second) return a.first > b.first;
+    return a.second > b.second;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    //map<string, priority_queue<int>> pqm;
+    map<string, map<int, int>> pqm;
+    map<string, int> gen_sum;
+
+    for (int i = 0; i < plays.size(); i++) {
+        if (pqm.find(genres[i]) == pqm.end()) {
+            map<int, int> pm;
+            pqm.insert({ genres[i], pm });
+            gen_sum.insert({ genres[i], 0 });
+        }
+        pqm[genres[i]].insert({ i, plays[i] });
+        gen_sum[genres[i]] += plays[i];
+    }
+
+    vector<pair<string, int>> vec(gen_sum.begin(), gen_sum.end());
+
+    sort(vec.begin(), vec.end(), cmp_si);
+    for (int i = 0; i < vec.size(); i++) {
+        vector<pair<int, int>> idxVec(pqm[vec[i].first].begin(), pqm[vec[i].first].end());
+        sort(idxVec.begin(), idxVec.end(), cmp_ii);
+        answer.push_back(idxVec[0].first);
+        if(idxVec.size() > 1)
+            answer.push_back(idxVec[1].first);
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<string> gv;
+    vector<int> pv;
+
+    gv.push_back("classic");
+    gv.push_back("pop");
+    gv.push_back("classic");
+    gv.push_back("classic");
+    gv.push_back("pop");
+
+    pv.push_back(500);
+    pv.push_back(600);
+    pv.push_back(150);
+    pv.push_back(800);
+    pv.push_back(2500);
+
+    vector<int> result = solution(gv, pv);
+
+    for (int i = 0; i < result.size(); i++) {
+        printf("%d, ",result[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
