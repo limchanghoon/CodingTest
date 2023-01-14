91dbbf309d64e87dfe4847b4354b2d6e23105f5c
limchanghoon <52607063+limchanghoon@users.noreply.github.com>
2023-01-14 19:49:42 +0900

    Add files via upload

--- LEVEL 3/L3_단어변환.cpp
+++ LEVEL 3/L3_단어변환.cpp
@@ -4,49 +4,56 @@
 
 using namespace std;
 
-// 너비우선 탐색
+class W {
+public:
+    W(string _begin, vector<string> _words) {
+        begin = _begin;
+        words.assign(_words.begin(), _words.end());
+        cnt = 0;
+    }
+
+    W(string _begin, vector<string> _words, int erase, int _cnt) {
+        begin = _begin;
+        _words.erase(_words.begin() + erase);
+        words = _words;
+        cnt = _cnt;
+    }
+
+    string begin;
+    vector<string> words;
+    int cnt;
+};
+
+// 너비 우선 탐색
 int solution(string begin, string target, vector<string> words) {
-    queue<string> q1;
-    queue<vector<string>> q2;
-    queue<int> q3;
-    q1.push(begin);
-    q2.push(words);
-    q3.push(0);
-    while (!q1.empty()) {
-        string curStr = q1.front();
-        vector<string> curWords = q2.front();
-        int cnt = q3.front();
+    queue<W> q;
 
-        q1.pop();
-        q2.pop();
-        q3.pop();
+    q.push(W(begin, words));
 
+    while (!q.empty()) {
+        W w = q.front();
+        q.pop();
         int count = 0;
-        for (int i = 0; i < curWords.size(); i++) {
+        for (int i = 0; i < w.words.size(); i++) {
             count = 0;
             for (int j = 0; j < begin.size(); j++) {
-                if (curStr[j] != curWords[i][j]) {
+                if (w.begin[j] != w.words[i][j]) {
                     if (++count > 1)
                         break;
                 }
             }
             if (count == 1) {
-                if (curWords[i].compare(target) == 0)
-                    return cnt + 1;
-                q1.push(curWords[i]);
-                vector<string> v = curWords;
-                v.erase(v.begin() + i);
-                q2.push(v);
-                q3.push(cnt + 1);
+                if (w.words[i].compare(target) == 0)
+                    return w.cnt + 1;
+                q.push(W(w.words[i], w.words, i, w.cnt + 1));
             }
         }
     }
-
     return 0;
 }
 
 int main() {
-    printf("%d\n", solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" }));
+    printf("%d\n",solution("hit", "cog", {"hot", "dot", "dog", "lot", "log", "cog"}));
     printf("%d\n", solution("hit", "cog", { "hot", "dot", "dog", "lot", "log" }));
     return 0;
 }
