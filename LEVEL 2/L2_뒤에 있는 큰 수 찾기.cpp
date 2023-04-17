8c0eca7272e3c5b0dec744388b1eec10df517b17
limchanghoon <52607063+limchanghoon@users.noreply.github.com>
2023-04-17 16:54:54 +0900

    Add files via upload

--- LEVEL 2/L2_뒤에 있는 큰 수 찾기.cpp
+++ LEVEL 2/L2_뒤에 있는 큰 수 찾기.cpp
@@ -2,9 +2,12 @@
 #include <vector>
 #include <iostream>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
+/*
+// 첫번째 풀이
 struct PR {
     int num, index;
     PR(int _num, int _index) : num(_num), index(_index) {}
@@ -30,6 +33,26 @@ vector<int> solution(vector<int> numbers) {
             }
         }
         q.push(PR(numbers[i], i));
+    }
+    return answer;
+}
+*/
+
+// 좀더 효율적인 풀이
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer(numbers.size(), -1);
+    stack<int> s;   // 요소가 인덱스
+    for (size_t i = 0; i < numbers.size(); i++) {
+        while (!s.empty()) {
+            if (numbers[s.top()] < numbers[i]) {
+                answer[s.top()] = numbers[i];
+                s.pop();
+            }
+            else {
+                break;
+            }
+        }
+        s.push(i);
     }
     return answer;
 }
